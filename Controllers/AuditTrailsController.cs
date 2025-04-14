153cffd4f21385aa9a2442367c380ed56218accc
frankkalengay@gmail.com <frankkalengay@gmail.com>
2025-04-14 23:59:49 +0200

    Added Admin Dashboard

--- Controllers/AuditTrailsController.cs
+++ Controllers/AuditTrailsController.cs
@@ -1,14 +1,10 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-using Microsoft.AspNetCore.Mvc;
+﻿using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.Rendering;
 using Microsoft.EntityFrameworkCore;
 using HelpDeskSystem.Data;
 using HelpDeskSystem.Models;
 
-namespace HelpDeskSystem.Views
+namespace HelpDeskSystem.Controllers
 {
     public class AuditTrailsController : Controller
     {
