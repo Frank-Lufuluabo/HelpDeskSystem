1c5a799285e4d8b2ade64cb7e989c8da632db3f3
frankkalengay@gmail.com <frankkalengay@gmail.com>
2025-06-25 23:55:42 +0200

    Added Roles views

--- ViewModels/RolesViewModel.cs
+++ ViewModels/RolesViewModel.cs
@@ -1,6 +1,15 @@
-﻿namespace HelpDeskSystem.ViewModels
+﻿using System.ComponentModel;
+
+namespace HelpDeskSystem.ViewModels
 {
     public class RolesViewModel
     {
+
+        [DisplayName("Role No")]
+        public string Id { get; set; }
+
+
+        [DisplayName("Role Name")]
+        public string RoleName { get; set; }
     }
 }
