78c04fc47e5b2e0b90f44e2bf06eb23cc4440637
frankkalengay@gmail.com <frankkalengay@gmail.com>
2025-07-10 23:42:40 +0200

    Add Services features

---
+++ Controllers/PdfExportsController.cs
@@ -0,0 +1,73 @@
+﻿using AutoMapper;
+using HelpDeskSystem.ClaimsManagement;
+using HelpDeskSystem.Data;
+using HelpDeskSystem.Interfaces;
+using HelpDeskSystem.ViewModels;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+
+namespace HelpDeskSystem.Controllers
+{
+    public class PdfExportsController : Controller
+    {
+        private readonly ApplicationDbContext _context;
+        private readonly IConfiguration _configuration;
+        private readonly IExportService _exportService;
+        private readonly IMapper _mapper;
+
+        public PdfExportsController(ApplicationDbContext context, IExportService exportService, IConfiguration configuration,
+            IMapper imapper)
+        {
+            _context = context;
+            _configuration = configuration;
+            _mapper = imapper;
+            _exportService = exportService;
+        }
+        public async Task<ActionResult> RecentTickets(TicketViewModel vm)
+        {
+            var alltickets = _context.Tickets
+                .Include(t => t.CreatedBy)
+                .Include(t => t.SubCategory)
+                .Include(t => t.Priority)
+                .Include(t => t.Status)
+                .Include(t => t.TicketComments)
+                .OrderBy(x => x.CreatedOn)
+                .AsQueryable();
+
+            if (vm != null)
+            {
+                if (!string.IsNullOrEmpty(vm.Title))
+                {
+                    alltickets = alltickets.Where(x => x.Title == vm.Title);
+                }
+
+                if (!string.IsNullOrEmpty(vm.CreatedById))
+                {
+                    alltickets = alltickets.Where(x => x.CreatedById == vm.CreatedById);
+                }
+
+                if (vm.StatusId > 0)
+                {
+                    alltickets = alltickets.Where(x => x.StatusId == vm.StatusId);
+                }
+
+                if (vm.PriorityId > 0)
+                {
+                    alltickets = alltickets.Where(x => x.PriorityId == vm.PriorityId);
+                }
+
+                if (vm.CategoryId > 0)
+                {
+                    alltickets = alltickets.Where(x => x.SubCategory.CategoryId == vm.CategoryId);
+                }
+            }
+
+            vm.Tickets = await alltickets.ToListAsync();
+          
+
+            return View(vm);    
+        }
+
+    }
+}
